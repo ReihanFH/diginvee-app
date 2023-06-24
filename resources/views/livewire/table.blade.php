482a70eef5e816bf99eae6c981709079a6eedeba
unknown <reihanhanif56@gmail.com>
2023-06-24 07:27:55 +0700

    update table share button

--- resources/views/livewire/table.blade.php
+++ resources/views/livewire/table.blade.php
@@ -88,12 +88,16 @@
                                         class="col-auto d-inline-block btn btn-md app-btn-tertiary m-1" target="_blank">
                                         <i class="bi bi-envelope-open"></i>
                                     </a>
-                                    <a type="button"
-                                        href="https://api.whatsapp.com/send?phone={{ $guest->phone }}&text={{ rawurlencode($saved_top_message) . '%0A%0A' . rawurlencode($guest->name) . '%0A%0A' . rawurlencode($saved_body_message) . '%0A%0A' . rawurlencode('https://arinindra.diginvee.com/' . $guest->code) . '%0A%0A' . rawurlencode($saved_bottom_message) }}"
-                                        title="Share" class="col-auto d-inline-block btn btn-md app-btn-tertiary m-1"
-                                        target="_blank">
-                                        <i class="bi bi-share"></i>
-                                    </a>
+
+                                    @if ($guest->phone !== '62')
+                                        <a type="button"
+                                            href="https://api.whatsapp.com/send?phone={{ $guest->phone }}&text={{ rawurlencode($saved_top_message) . '%0A%0A' . rawurlencode($guest->name) . '%0A%0A' . rawurlencode($saved_body_message) . '%0A%0A' . rawurlencode('https://arinindra.diginvee.com/' . $guest->code) . '%0A%0A' . rawurlencode($saved_bottom_message) }}"
+                                            title="Share"
+                                            class="col-auto d-inline-block btn btn-md app-btn-tertiary m-1"
+                                            target="_blank">
+                                            <i class="bi bi-share"></i>
+                                        </a>
+                                    @endif
                                     <button type="button" data-bs-toggle="modal" data-bs-target="#updateGuestModal"
                                         wire:click="editGuest({{ $guest->id }})" title="Edit Guest"
                                         class="col-auto d-inline-block btn btn-md app-btn-tertiary m-1">
