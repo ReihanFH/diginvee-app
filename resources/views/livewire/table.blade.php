8c6e8ffdffe7a4967c0ac444ff0ba0f3aaba36e4
unknown <reihanhanif56@gmail.com>
2023-06-25 12:08:00 +0700

    update phone form

--- resources/views/livewire/table.blade.php
+++ resources/views/livewire/table.blade.php
@@ -97,6 +97,14 @@
                                             @if ($saved_bottom_message !== null)
                                                 @if ($guest->phone !== '62')
                                                     <a type="button"
+                                                        href="https://api.whatsapp.com/send?phone=&text={{ rawurlencode($saved_top_message) . '%0A' . rawurlencode($guest->name) . '%0A%0A' . rawurlencode($saved_body_message) . '%0A%0A' . rawurlencode('https://arinindra.diginvee.com/' . $guest->code) . '%0A%0A' . rawurlencode($saved_bottom_message) }}"
+                                                        title="Share"
+                                                        class="col-auto d-inline-block btn btn-md app-btn-tertiary m-1"
+                                                        target="_blank">
+                                                        <i class="bi bi-share"></i>
+                                                    </a>
+                                                @else
+                                                    <a type="button"
                                                         href="https://api.whatsapp.com/send?phone={{ $guest->phone }}&text={{ rawurlencode($saved_top_message) . '%0A' . rawurlencode($guest->name) . '%0A%0A' . rawurlencode($saved_body_message) . '%0A%0A' . rawurlencode('https://arinindra.diginvee.com/' . $guest->code) . '%0A%0A' . rawurlencode($saved_bottom_message) }}"
                                                         title="Share"
                                                         class="col-auto d-inline-block btn btn-md app-btn-tertiary m-1"
