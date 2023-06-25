84e529b42d92b4cf156197370e9977a1a4d5c369
unknown <reihanhanif56@gmail.com>
2023-06-25 08:18:02 +0700

    add margin into head button

--- resources/views/livewire/admin.blade.php
+++ resources/views/livewire/admin.blade.php
@@ -42,11 +42,11 @@
 
             <div class="row my-4 justify-content-end">
                 <div class="col-12">
-                    <button type="button" class="btn app-btn-primary float-end ms-2" data-bs-toggle="modal"
+                    <button type="button" class="btn app-btn-primary float-end my-2 ms-2" data-bs-toggle="modal"
                         data-bs-target="#importModal">
                         <i class="bi bi-plus-lg"></i> Import Guest
                     </button>
-                    <button type="button" class="btn app-btn-primary float-end ms-2" data-bs-toggle="modal"
+                    <button type="button" class="btn app-btn-primary float-end my-2 ms-2" data-bs-toggle="modal"
                         data-bs-target="#guestModal">
                         <i class="bi bi-plus-lg"></i> New Guest
                     </button>
